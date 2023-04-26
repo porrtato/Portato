0faca454ec06f48c1a58e25e92a4ef18555faf48
TimoSanakim <63639525+TimoSanakim@users.noreply.github.com>
2023-04-26 18:44:24 +0200

    images 2 fit

--- src/HighlightedItem.js
+++ src/HighlightedItem.js
@@ -1,7 +1,30 @@
 import React from 'react'
+
+import Grid from '@mui/material/Grid';
+
 import './App.css';
 import './images/images.css'
 
+
+const styles = {
+  item1: {
+    backgroundColor: '#FBEC39',
+    height: '100px',
+  },
+  innerGrid: {
+    height: '40px',
+    backgroundColor: '#F6546A',
+  },
+  item4: {
+    backgroundColor: '#A4ACDA',
+    height: '100px',
+  },
+  item5: {
+    backgroundColor: '#2A009D',
+    height: '100px',
+    color: '#ffffff',
+  },
+};
 
 function HighlightedItem() {
   return (
@@ -9,7 +32,19 @@ function HighlightedItem() {
     <div className='HighlightedItem boxItem'>
       <h1>Aqua the water goddess</h1>
       <p>Newest Osu skin</p>
-      <div className='aquaTheWaterGoddess'></div>
+      <Grid className='contain'container spacing={2}>
+        <Grid item md={6}>
+          <div className='testImage'></div>
+        </Grid>
+        
+
+        <Grid item md={6}>
+        <div className='testImage'></div>
+        </Grid>
+
+      </Grid>
+
+      
     </div>
 
   )
